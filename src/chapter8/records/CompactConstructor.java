7f72e6dcac01a21c710748765e126790941823f3
Michiel <michiel.desmet@purplecatsoftware.com>
2024-06-28 07:16:36 +0200

    added record constructors example

---
+++ src/chapter8/records/CompactConstructor.java
@@ -0,0 +1,20 @@
+package chapter8.records;
+
+public record CompactConstructor(int number, String name) {
+    public CompactConstructor {
+        if (number < 0) {
+            throw new IllegalArgumentException();
+        }
+        name = name.toUpperCase();
+    }
+
+
+    public static void main(String[] args) {
+        var compactConstructor = new CompactConstructor(5, "CompactConstructor");
+        System.out.println(compactConstructor.number);
+        System.out.println(compactConstructor.name);
+
+        //Throws exception
+        new CompactConstructor(-1, "CompactConstructor");
+    }
+}
---
+++ src/chapter8/records/LongConstructor.java
@@ -0,0 +1,26 @@
+package chapter8.records;
+
+public record LongConstructor(int number, String name) {
+
+    public LongConstructor(int number, String name) {
+        if (number < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.number = number;
+        this.name = name.toUpperCase();
+    }
+
+    // Modifying the values passed, it is NOT possible to change the fields in the record.
+    //    public LongConstructor {
+    //        if(name == null || name.isEmpty()) {
+    //            throw new IllegalArgumentException();
+    //        }
+    //        name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    //    }
+
+    public static void main(String[] args) {
+        var longConstructor = new LongConstructor(10, "voorbeeld van een naam");
+        System.out.println(longConstructor.number);
+        System.out.println(longConstructor.name);
+    }
+}
